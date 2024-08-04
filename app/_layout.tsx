ef6c8001fb038f28aaf8bbf15c3082d4787f593c
CatAvadani <catalinaavadani@gmail.com>
2024-08-04 22:28:24 +0300

    Add FavoritesContextProvider for managing favorite show ids

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,6 +10,7 @@ import { useEffect } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import FavoritesContextProvider from '@/store/Favorites-context';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -36,11 +37,13 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <QueryClientProvider client={queryClient}>
-        <Stack>
-          <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
-          <Stack.Screen name='index' options={{ headerShown: false }} />
-          <Stack.Screen name='+not-found' />
-        </Stack>
+        <FavoritesContextProvider>
+          <Stack>
+            <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
+            <Stack.Screen name='index' options={{ headerShown: false }} />
+            <Stack.Screen name='+not-found' />
+          </Stack>
+        </FavoritesContextProvider>
       </QueryClientProvider>
     </ThemeProvider>
   );
